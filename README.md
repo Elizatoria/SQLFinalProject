b081bfca9bc569a344db46565775d6aaee23f7d7
Elizatoria <Elizatoria@gmail.com>
2024-01-08 02:56:37 -0500

    Database Made

--- README.md
+++ README.md
@@ -5,3 +5,7 @@ The startup company I chose will be an artist doing artwork for commissions.
 
 Database Diagram
 https://app.quickdatabasediagrams.com/#/d/T10lf8
+
+Used https://cobbl.io/ to make the Customer.csv File.
+
+Used https://www.mockaroo.com/ to make the Orders.csv File.
--- SQL Queries/Database.sql
+++ SQL Queries/Database.sql
@@ -1,1 +1,11 @@
 /* Create Final Project Database */
+-- USE master
+-- GO
+-- CREATE DATABASE FinalProject
+
+/* Use Microsoft SQL Server Management Studio to add CSV Files to Database */
+USE FinalProject
+GO
+
+ALTER TABLE Orders
+ALTER COLUMN Phone NVARCHAR(50) NOT NULL FOREIGN KEY REFERENCES Customer(Phone)
